544f94c7785789c026e71012f91eaffa8bdfa712
baron7151 <ibaron.715@gmail.com>
2019-09-15 15:44:04 +0900

    ch5

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    
+    # User Management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local app
+    path('account/', include('users.urls')),
+    path('', include('pages.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignupPageView(generic.CreateView):
+    form_class = CustomUserCreationForm #  参照するフォームのクラスを指定
+    success_url = reverse_lazy('login') #  送信されるとsuccess_urlにリダイレクトされる
+    template_name = 'signup.html' #  表示するテンプレートのファイル名
