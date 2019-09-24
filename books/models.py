997e9df7be89c31e58701eb7c6a4468a1103a245
baron7151 <ibaron.715@gmail.com>
2019-09-25 00:19:25 +0900

    アップロード機能を追加

--- books/models.py
+++ books/models.py
@@ -9,7 +9,8 @@ class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='cover/', blank=True)
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
