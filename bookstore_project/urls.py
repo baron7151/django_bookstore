583f0c687f4ed88d52070db9bd31386eb8ae51cd
baron7151 <ibaron.715@gmail.com>
2019-09-28 17:43:15 +0900

    .gitignoreを追加

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     # Local app
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
+    path('orders/', include('orders.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
